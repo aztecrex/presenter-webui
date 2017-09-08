7a6ea161a9b5f911478681a602bff8e0c078bebc
Greg Wiley <aztec.rex@jammm.com>
2017-09-07 20:31:31 -0700

    checkpoint test

---
+++ test/Test.purs
@@ -0,0 +1,25 @@
+module Test.Main where
+
+import Prelude
+
+import Test.Unit (suite, test, timeout)
+import Test.Unit.Main (runTest)
+import Test.Unit.Assert as Assert
+
+import Node.FS.Aff as FS
+import Node.Encoding (Encoding(..))
+
+
+main = runTest do
+  suite "sync code" do
+    test "arithmetic" do
+      Assert.assert "2 + 2 should be 4" $ (2 + 2) == 4
+      Assert.assertFalse "2 + 2 shouldn't be 5" $ (2 + 2) == 5
+      Assert.equal 4 (2 + 2)
+      Assert.expectFailure "2 + 2 shouldn't be 5" $ Assert.equal 5 (2 + 2)
+  suite "sync code 2" do
+    test "arithmetic" do
+      Assert.assert "2 + 2 should be 4" $ (2 + 2) == 4
+      Assert.assertFalse "2 + 2 shouldn't be 5" $ (2 + 2) == 5
+      Assert.equal 4 (2 + 2)
+      Assert.expectFailure "2 + 2 shouldn't be 5" $ Assert.equal 5 (2 + 2)
