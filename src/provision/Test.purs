4bf54585b28f63dc67f48252b8703e12394b6013
Greg Wiley <aztec.rex@jammm.com>
2017-09-10 14:31:46 -0700

    checkpoint

---
+++ src/provision/Test.purs
@@ -0,0 +1,29 @@
+module Provision.Test ( provisionTest ) where
+
+import Prelude
+import Control.Monad.Eff (Eff)
+import Control.Monad.Eff.Console (CONSOLE)
+import Control.Monad.Aff.AVar (AVAR)
+import Test.Unit (suite, test)
+import Test.Unit.Console (TESTOUTPUT)
+import Test.Unit.Main (runTest)
+import Test.Unit.Assert as Assert
+
+provisionTest :: ∀ fx. Eff ( console :: CONSOLE
+                  , testOutput :: TESTOUTPUT
+                  , avar :: AVAR
+                  | fx
+          ) Unit
+provisionTest = runTest do
+  suite "sync code" do
+    test "arithmetic" do
+      Assert.assert "2 + 2 should be 4" $ (2 + 2) == 4
+      Assert.assertFalse "2 + 2 shouldn't be 5" $ (2 + 2) == 5
+      Assert.equal 4 (2 + 2)
+      Assert.expectFailure "2 + 2 shouldn't be 5" $ Assert.equal 5 (2 + 2)
+  suite "sync code 2" do
+    test "arithmetic" do
+      Assert.assert "2 + 2 should be 4" $ (2 + 2) == 4
+      Assert.assertFalse "2 + 2 shouldn't be 5" $ (2 + 2) == 5
+      Assert.equal 4 (2 + 2)
+      Assert.expectFailure "2 + 2 shouldn't be 5" $ Assert.equal 5 (2 + 2)
