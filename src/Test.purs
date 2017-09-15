e48cf351565611f2496c97896d0e396262bdf06b
Greg Wiley <aztec.rex@jammm.com>
2017-09-15 10:05:19 -0700

    checkpoint

--- src/Test.purs
+++ src/Test.purs
@@ -1,19 +1,16 @@
 module Test.Main where
 
--- import Prelude
--- import Control.Monad.Eff (Eff)
--- import Control.Monad.Eff.Console (CONSOLE)
--- import Control.Monad.Aff.AVar (AVAR)
--- import Test.Unit (suite, test)
--- import Test.Unit.Console (TESTOUTPUT)
--- import Test.Unit.Main (runTest)
--- import Test.Unit.Assert as Assert
+import Prelude
+import Control.Monad.Eff (Eff)
+import Control.Monad.Eff.Console (CONSOLE)
+import Control.Monad.Aff.AVar (AVAR)
+import Test.Unit.Console (TESTOUTPUT)
 import Provision.Test (provisionTest)
 
--- main :: ∀ fx. Eff ( console :: CONSOLE
---                   , testOutput :: TESTOUTPUT
---                   , avar :: AVAR
---                   | fx
---           ) Unit
+main :: ∀ fx. Eff ( console :: CONSOLE
+                  , testOutput :: TESTOUTPUT
+                  , avar :: AVAR
+                  | fx
+          ) Unit
 main = do
   provisionTest
