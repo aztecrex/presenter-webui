f6b23f66867d23b4c06ec3797d8a298b6ab74ed7
Greg Wiley <aztec.rex@jammm.com>
2017-09-16 12:10:18 -0700

    messing with marked

--- src/Test.purs
+++ src/Test.purs
@@ -2,10 +2,34 @@ module Test.Main where
 
 import Prelude
 import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Console (CONSOLE)
+import Control.Monad.Eff.Console (CONSOLE, log)
 import Control.Monad.Aff.AVar (AVAR)
 import Test.Unit.Console (TESTOUTPUT)
 import Provision.Test (provisionTest)
+import Content.Markdown (go, go2)
+
+
+text :: String
+text = """
+# this is cool
+
+we need some
+
+- thing 1
+  - thing 1.1
+  - thing 1.2
+- thing 2
+
+---
+
+# another page
+
+
+here is some text spanning
+multiple lines forming
+a paragrah with a [link](http://gregwiley.com).
+
+"""
 
 main :: ∀ fx. Eff ( console :: CONSOLE
                   , testOutput :: TESTOUTPUT
@@ -13,4 +37,7 @@ main :: ∀ fx. Eff ( console :: CONSOLE
                   | fx
           ) Unit
 main = do
-  provisionTest
+  -- provisionTest
+  log text
+  pure $ go text
+  pure $ go2 text
--- src/content/Markdown.purs
+++ src/content/Markdown.purs
@@ -1,12 +1,16 @@
-module Content.Markdown (proto) where
+module Content.Markdown (proto, go, go2) where
 
 
 -- import Text.Markdown.SlamDown
 -- import Text.Markdown.SlamDown.Parser
 
+
 import Prelude (Unit)
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
+
+foreign import go :: String -> Unit
+foreign import go2 :: String -> Unit
 
 proto :: ∀ fx. Eff ( console :: CONSOLE | fx ) Unit
 proto = do
