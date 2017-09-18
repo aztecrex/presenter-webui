f61cd00bb24558466825109bc310bfbd45ed6bc2
Greg Wiley <aztec.rex@jammm.com>
2017-09-17 21:03:20 -0700

    install slides into state

--- src/model/State.Test.purs
+++ src/model/State.Test.purs
@@ -1,15 +1,20 @@
 module Model.State.Test (tests) where
 
-import Prelude (Unit, not, ($))
+import Prelude (Unit, not, ($), id, map, const, discard)
+import Data.List (List(..))
+import Data.Either (either)
+import Text.Markdown.SlamDown (SlamDown)
+import Text.Markdown.SlamDown.Parser (parseMd)
+import Content.Slide (slides)
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE)
 import Control.Monad.Aff.AVar (AVAR)
 import Test.Unit (suite, test)
 import Test.Unit.Main (runTest)
-import Test.Unit.Assert (assert)
+import Test.Unit.Assert (assert, equal)
 import Test.Unit.Console (TESTOUTPUT)
 
-import Model.State(initial, presentation, presentable)
+import Model.State(initial, presentation, presentable, install, size)
 
 tests :: ∀ fx. Eff ( console :: CONSOLE
                   , testOutput :: TESTOUTPUT
@@ -21,3 +26,37 @@ tests = do
     suite "Model.State" do
       test "initial presentation is not presentable" do
         assert "should be not presentable" $ not $ presentable $ presentation initial
+      test "install slides" do
+        let newState = install testSource initial
+        let pres = presentation newState
+        let actual = size pres
+        let expected = 3
+        equal expected actual
+
+
+testSlides :: List SlamDown
+testSlides = either (const Nil) id $ map slides $ parseMd testSource
+
+testSource :: String
+testSource = """
+# Slide One
+
+On this slide we have lots of coolness.
+
+---
+# Slide Two
+
+## Let me tell you
+
+This is weird.
+
+## Let me tell you something else
+
+_Really_ weird.
+
+---
+# Final Slide
+
+Wasn't that just the greatest presentation?
+
+"""
--- src/model/State.purs
+++ src/model/State.purs
@@ -1,13 +1,18 @@
 module Model.State (
     initial,
     presentation,
-    presentable
+    presentable,
+    install,
+    size
     ) where
 
-import Data.NonEmpty (NonEmpty)
+import Prelude (map, ($), (+))
+import Data.NonEmpty (NonEmpty, (:|))
 import Data.Either (Either(..), isRight)
-import Data.List (List)
+import Data.List (List(..), (:), length)
+import Content.Slide (slides)
 import Text.Markdown.SlamDown (SlamDown)
+import Text.Markdown.SlamDown.Parser (parseMd)
 
 type PresentationError = String
 
@@ -31,3 +36,16 @@ presentation = _.presentation
 
 presentable :: Presentation -> Boolean
 presentable = isRight
+
+createPresentation :: String -> Presentation
+createPresentation source = case map slides $ parseMd source of
+  Left _ -> Left "parse error"
+  Right Nil -> Left "no content"
+  Right (s : ss) -> Right { index: 10000, slides: s :| ss }
+
+install :: String -> State -> State
+install source state = state { presentation = createPresentation source}
+
+size :: Presentation -> Int
+size (Left _) = 0
+size (Right { slides: (e :| es) }) = length es + 1
