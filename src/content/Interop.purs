5bc49824fa77146df7c6441a96944620379142a9
Greg Wiley <aztec.rex@jammm.com>
2017-09-22 20:24:29 -0700

    source getter

--- src/content/Interop.purs
+++ src/content/Interop.purs
@@ -1,36 +1,18 @@
-module Interop where
+module Content.Interop (getSource) where
 
-import Prelude (bind, pure, show, ($), (<<<))
-import Data.Either (Either(..), either)
-import Data.Maybe (Maybe(..))
+import Prelude (bind, pure, show, ($))
+import Data.Either (either)
 import Control.Monad.Aff (attempt, Aff)
 import Network.HTTP.Affjax (AJAX, get)
-import Data.Argonaut (class DecodeJson, decodeJson, (.?))
 
 
-type Todos = Array Todo
-
-newtype Todo = Todo
-  { id :: Int
-  , title :: String }
-
-data Event = RequestTodos | ReceiveTodos (Either String Todos)
-
-instance decodeJsonTodo :: DecodeJson Todo where
-  decodeJson json = do
-    obj <- decodeJson json
-    id <- obj .? "id"
-    title <- obj .? "title"
-    pure $ Todo { id: id, title: title }
-
-getSomething :: forall r.
+getSource :: forall r.
       Aff
         ( ajax :: AJAX
         | r
         )
-        (Maybe Event)
-getSomething =  do
-      res <- attempt $ get "http://jsonplaceholder.typicode.com/users/1/todos"
-      let decode r = decodeJson r.response :: Either String Todos
-      let todos = either (Left <<< show) decode res
-      pure $ Just $ ReceiveTodos todos
+        String
+getSource = do
+  res <- attempt $ get "/functional-and-serverless.present.md"
+  let decode r = r.response
+  pure $ either show decode res
